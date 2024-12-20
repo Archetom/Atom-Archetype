c462aa93dceeeae9042d1ef6f80d9c2b09ace56d
fuhanfeng <michael.fhf@gmail.com>
2024-12-20 16:10:04 +0800

    feat:更新依赖，修正 import error

--- src/main/resources/archetype-resources/shared/src/main/java/shared/enums/EventEnum.java
+++ src/main/resources/archetype-resources/shared/src/main/java/shared/enums/EventEnum.java
@@ -3,7 +3,6 @@ package ${package}.shared.enums;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.Objects;
 
